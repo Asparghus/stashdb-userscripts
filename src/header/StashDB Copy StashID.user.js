6d7a1c5987d49095dd83458c648d7b66b8f607b5
Asparghus <147214977+Asparghus@users.noreply.github.com>
2023-12-21 19:26:55 +0000

    Update StashDB Copy StashID.user.js

--- src/header/StashDB Copy StashID.user.js
+++ src/header/StashDB Copy StashID.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name        StashDB Copy StashID
-// @namespace   https://github.com/7dJx1qP/stashdb-userscripts
+// @namespace   https://github.com/Asparghus/stashdb-userscripts
 // @description StashDB Copy StashID
 // @version     0.1.0
 // @author      7dJx1qP
 // @match       https://stashdb.org/*
-// @resource    IMPORTED_CSS https://raw.githubusercontent.com/7dJx1qP/stashdb-userscripts/dist/public/scene.css
+// @resource    IMPORTED_CSS https://raw.githubusercontent.com/Asparghus/stashdb-userscripts/dist/public/scene.css
 // @grant       unsafeWindow
 // @grant       GM_setClipboard
 // @grant       GM_getResourceText
